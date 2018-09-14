8cd0729cfd553bbdbe56d6257d570562c13ea7fb
pornpunnarai <kudokung_w@hotmail.com>
2018-09-14 14:24:33 +0700

    fix shopping cart

--- index.php
+++ index.php
@@ -52,6 +52,7 @@ session_start();
             height: 3rem;
             width: 3rem;
         }
+
     </style>
 
 </head>
@@ -270,7 +271,7 @@ session_start();
                 </div>
 
                 <div class="header-cart-buttons flex-w w-full">
-                    <a href="shoping-cart.php" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
+                    <a href="cart.php" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
                         View Cart
                     </a>
 
